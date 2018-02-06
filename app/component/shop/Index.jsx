d20b9acd65d57fa60d2fc0c2e950713254a53657
王军-PC\王军 <296188842@qq.com>
2018-02-06 17:35:17 +0800

    first commit

---
+++ app/component/shop/Index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../../public/css/index.pcss'
+import Seconds from '../common/Seconds'
+
+// 提取公共组件
+const Index = () =>
+    <Seconds title="商城" />
+    ;
+
+export default Index;
